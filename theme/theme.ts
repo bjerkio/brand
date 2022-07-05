84c8e880c4fd4a4fdd107fb409834471545cc2da
moripen <markusmoripen@gmail.com>
2022-07-05 19:02:39 +0200

    fix: connections to declarations
    * fix: connections to declarations added to theme

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,9 +1,10 @@
 import { makeTheme } from '@theme-ui/css/utils';
-import { text } from './text';
 import { buttons } from './buttons';
+import { colors } from './colors';
+import { layout } from './layout';
+import { links } from './links';
 import { styles } from './styles';
-
-const baseFontSize = 16;
+import { text } from './text';
 
 export const theme = makeTheme({
   space: [8, 16, 24, 32, 48, 64, 96, 128, 192, 256],
@@ -11,26 +12,7 @@ export const theme = makeTheme({
     container: 1500,
     readable: 940,
   },
-  colors: {
-    access: '#4AD97B',
-    blue20: '#C9D1FF',
-    blue40: '#8FA1FF',
-    blue60: '#7087FF',
-    blue80: '#4B5CF0',
-    blue100: '#2A3BCC',
-    dark20: '#B7BCC6',
-    dark40: '#878F9F',
-    dark60: '#576379',
-    dark80: '#273653',
-    dark100: '#0F2040',
-    error: '#DC3B58',
-    green20: '#EDF9F1',
-    green40: '#D2FBE0',
-    green60: '#BCF9C6',
-    green80: '#A6F7AE',
-    green100: '#90F494',
-    warning: '#FFD464',
-  },
+  colors,
   fonts: {
     body: 'Sora',
     heading: 'Sora',
@@ -41,6 +23,7 @@ export const theme = makeTheme({
     heading: 600,
   },
   fontSizes: [16, 18, 20, 24, 28, 32, 48],
+  links,
   text,
   buttons,
   badges: {},
@@ -50,6 +33,7 @@ export const theme = makeTheme({
       gap: 2,
     },
   },
+  layout,
   cards: {
     primary: {
       display: 'flex',
